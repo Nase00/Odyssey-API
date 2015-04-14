3a2b4982c5674b36c779cdaec34a022c869d6002
nase00 <nase00@gmail.com>
2015-04-14 02:19:21 -0500

    Trip controller refactor.

--- app/controllers/trips.rb
+++ app/controllers/trips.rb
@@ -1,17 +1,27 @@
 get '/trip_for/:bike_id/after/:offset' do
-	origin_station = Station.find_by(station_id: trip.origin_station_id)
-	destination_station = Station.find_by(station_id: trip.destination_station_id)
+	if trip.nil? && params[:offset] == 0.to_s
+		{
+			status: 404,
+			message: "Bike not found"
+		}.to_json
+	elsif trip.nil?
+		{
+			status: 510,
+			message: "End of the line"
+		}.to_json
+	else
+		origin_station = Station.find_by(station_id: trip.origin_station_id)
+		destination_station = Station.find_by(station_id: trip.destination_station_id)
 
-	hash = {
-		lat: origin_station.latitude,
-		long: origin_station.longitude,
-		trip_id: trip.trip_id,
-		start_time: trip.start_time,
-		stop_time: trip.stop_time,
-		duration: trip.duration,
-		start_location: origin_station.name,
-		stop_location: destination_station.name
-	}
-
-	hash.to_json
+		{
+			lat: origin_station.latitude,
+			long: origin_station.longitude,
+			trip_id: trip.trip_id,
+			start_time: trip.start_time,
+			stop_time: trip.stop_time,
+			duration: trip.duration,
+			start_location: origin_station.name,
+			stop_location: destination_station.name
+		}.to_json
+	end
 end
