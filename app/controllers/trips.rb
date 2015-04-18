85a2269e7c96efc94a8b282b34e2f69a910aa463
nase00 <nase00@gmail.com>
2015-04-18 15:02:09 -0500

    Fix output to json.

--- app/controllers/trips.rb
+++ app/controllers/trips.rb
@@ -1,27 +1,29 @@
 get '/trip_for/:bike_id/after/:offset' do
-	if trip.nil? && params[:offset] == 0.to_s
-		{
+	if trip.nil? && params[:offset] == "0"
+		hash = {
 			status: 404,
 			message: "Bike not found"
-		}.to_json
+		}
 	elsif trip.nil?
-		{
+		hash = {
 			status: 510,
 			message: "End of the line"
-		}.to_json
+		}
 	else
 		origin_station = Station.find_by(station_id: trip.origin_station_id)
 		destination_station = Station.find_by(station_id: trip.destination_station_id)
 
-		{
+		hash = {
+			status: 200,
 			lat: origin_station.latitude,
-			long: origin_station.longitude,
+			lng: origin_station.longitude,
 			trip_id: trip.trip_id,
 			start_time: trip.start_time,
 			stop_time: trip.stop_time,
 			duration: trip.duration,
 			start_location: origin_station.name,
 			stop_location: destination_station.name
-		}.to_json
+		}
 	end
+	json(hash)
 end
